63807a5080300ff7a42c2eb3b9394a22b4bb871d
Snehil <snehilverma488@gmail.com>
2020-10-10 16:24:18 +0530

    [Snehil] Add : Test Case Given I Am In Any Mood Should Return HAPPY

--- MoodAnalyzerProblemTest/UnitTest1.cs
+++ MoodAnalyzerProblemTest/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace MoodAnalyzerProblemTest
             //Assert
             Assert.AreEqual("SAD", result);
         }
+        [TestMethod]
+        public void Given_I_Am_In_Any_Mood_Should_Return_HAPPY()
+        {
+            //Arrange
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string message = "I am in any mood.";
+            //Act
+            string result = moodAnalyser.AnalyseMood(message);
+            //Assert
+            Assert.AreEqual("HAPPY", result);
+        }
     }
 }
