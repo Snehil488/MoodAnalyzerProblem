f7c88047f367e7156d7b343d9df1da1323e29812
Snehil <snehilverma488@gmail.com>
2020-10-11 14:22:24 +0530

    [Snehil] Add : Test case Given Empty Mood Should Throw MoodAnalysisException

--- MoodAnalyzerProblemTest/UnitTest1.cs
+++ MoodAnalyzerProblemTest/UnitTest1.cs
@@ -46,5 +46,22 @@ namespace MoodAnalyzerProblemTest
                 Assert.AreEqual("Mood should not be null.", e.Message);
             }
         }
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indicating_Empty_Mood()
+        {
+            try
+            {
+                //Arrange
+                string message = "";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                //Act
+                string result = moodAnalyser.AnalyseMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                //Assert
+                Assert.AreEqual("Mood should not be empty.", e.Message);
+            }
+        }
     }
 }
